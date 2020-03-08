127366c547812fd43e78276ce1684a0187baa76a
Michelle Morin <morin2m@gmail.com>
2020-03-08 10:17:14 -0700

    add postcard route

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -22,5 +22,14 @@ namespace FriendLetter.Controllers
 
     [Route("/form")]
     public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
